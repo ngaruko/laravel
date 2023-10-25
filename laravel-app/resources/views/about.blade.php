2f6cfcf9bff1b0b33484c37a887916a9023c5a66
ngaruko <ngarukob@yahoo.com>
2023-10-25 00:29:23 +0000

    “adding layouts and components”

---
+++ laravel-app/resources/views/about.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.base')
+
+@section('user', 'Andrei')
+
+@section('content')
+<h1 class="text-4xl font-bold">About Laravel</h1>
+<p class="mt-4 text-lg">
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+    Eaque in saepe ea eius ex recusandae impedit earum! 
+    Aperiam quibusdam quos voluptate quisquam cum saepe incidunt odio delectus! Eius, obcaecati asperiores.</p>
+
+@endsection
---
+++ laravel-app/resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<x-base>
+
+<x-slot name="user">
+Mandela
+</x-slot>
+
+<x-slot name="content">
+<h1 class="text-4xl font-bold">Welcome Home</h1>
+<p class="mt-4 text-lg">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque in saepe ea eius ex recusandae impedit earum! Aperiam quibusdam quos voluptate quisquam cum saepe incidunt odio delectus! Eius, obcaecati asperiores.</p>
+</x-slot>
+</x-base>
+
